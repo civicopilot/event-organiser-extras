f5ebf414747cb160fc2729b3bf82fd0cfb1cd25c
Andy Burns <andy@andyburns.co>
2026-08-07 02:43:50 +0000

    Bump version to 1.1.1

--- event-organiser-extras.php
+++ event-organiser-extras.php
@@ -15,7 +15,7 @@
  * Plugin Name:       Event Organiser Extras
  * Plugin URI:        https://github.com/civicopilot/event-organiser-extras
  * Description:       Extends WPCV Event Organiser with shortcodes, meta sidebar with registration link logic
- * Version:           1.1.0
+ * Version:           1.1.1
  * Requires Plugins:  wpcv-event-organiser
  * Author:            Andy Burns
  * Author URI:        https://civicopilot.com/
@@ -33,7 +33,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'EVENT_ORGANISER_EXTRAS_VERSION', '1.1.0' );
+define( 'EVENT_ORGANISER_EXTRAS_VERSION', '1.1.1' );
 
 /**
  * Load the plugin text domain for translation.
